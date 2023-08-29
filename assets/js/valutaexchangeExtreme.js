093e840901d7dc4e80862fba23703a1213a74e18
MarlaRey <hejmarlenehej@gmail.com>
2023-08-29 13:21:58 +0200

    opgaverne er alle løst

--- assets/js/valutaexchangeExtreme.js
+++ assets/js/valutaexchangeExtreme.js
@@ -1,3 +1,38 @@
+// Find knappen i DOM
+const calculateButton = document.getElementById('calculateButt');
+
+// Tilføj en event listener til knappen
+calculateButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    gatherInputData();
+});
+
+// Funktion til at indsamle data fra formularen og kalde beregningsfunktionen
+function gatherInputData() {
+    // Hent værdierne fra input-felterne
+    const myEuroRate = parseFloat(document.getElementById('euroRate').value);
+    const myDollarRate = parseFloat(document.getElementById('dollarRate').value);
+    const myDanishAmount = parseFloat(document.getElementById('danishAmount').value);
+
+    // Kald beregningsfunktionen og send data med
+    const euroResult = calculateExchange(myDanishAmount, myEuroRate);
+    const dollarResult = calculateExchange(myDanishAmount, myDollarRate);
+
+    // Kald view-funktionen for at vise resultaterne
+    showResult(euroResult, dollarResult, myDanishAmount);
+}
+
+// Funktion til beregning af valutakurs
+function calculateExchange(amountDKK, exchangeRate) {
+    const exchangedAmount = amountDKK / exchangeRate;
+    return exchangedAmount;
+}
+
+// Vis resultatet i view-funktionen
+function showResult(myEuroResult, myDollarResult, myDanishAmount) {
+    const resultElement = document.getElementById('result');
+    resultElement.innerHTML = myDanishAmount + ' danske kroner bliver til: ' + myEuroResult + ' euro og ' + myDollarResult + ' dollars';
+}
 
 
 
--- assets/js/valutaexchangeThree.js
+++ assets/js/valutaexchangeThree.js
@@ -14,18 +14,41 @@ calculateButton.addEventListener('click', (e) => {
 
 
 // indsamling af data fra dom (model code fra MVC)
+// Funktion 1: gatherInputData
+function gatherInputData() {
+    // Hent værdierne fra DOM-elementerne
+    const myEuroRate = parseFloat(document.getElementById('euroRate').value);
+    const myDollarRate = parseFloat(document.getElementById('dollarRate').value);
+    const myDanishAmount = parseFloat(document.getElementById('danishAmount').value);
 
+    // Kald makeExchange funktionen med de indsamlede data
+    makeExchange(myEuroRate, myDollarRate, myDanishAmount);
+}
 
 
 
 
 // håndterer data behandling  (controller code fra MVC)
+// Funktion 2: makeExchange
+function makeExchange(myEuroRate, myDollarRate, myDanishAmount) {
+    // Kald calculateExchange to gange med forskellige vekselrater
+    const euroResult = calculateExchange(myDanishAmount, myEuroRate);
+    const dollarResult = calculateExchange(myDanishAmount, myDollarRate);
 
+    // Vis resultaterne ved at kalde showResult funktionen
+    showResult(euroResult, dollarResult, myDanishAmount);
+}
 
 
 // sub funktion der udfører en opgave og returnerer et resultat.  stadig controller code i MVC
+// Funktion 3: calculateExchange
+function calculateExchange(amountDKK, exchangeRate) {
+    // Beregn det udvekslede beløb
+    const exchangedAmount = amountDKK / exchangeRate;
 
-
+    // Returner det udvekslede beløb
+    return exchangedAmount;
+}
 
 
 // viser resultatet  (view code fra MVC)
